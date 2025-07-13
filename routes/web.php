98e4c125bc7d1998fb6a3304d10a6536e91feef6
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-13 16:32:36 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhookController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/webhook/{store}', [WebhookController::class, 'handle']);
+Route::post('/webhook/{store}', [WebhookController::class, 'handle'])->withoutMiddleware([VerifyCsrfToken::class]);
