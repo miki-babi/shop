9fd87e5bcb496af5baefa5c2e50d85efcbc3c31a
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-17 14:57:58 +0300

    test

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -14,7 +14,7 @@
     <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
         <h2 class="text-2xl font-bold mb-6 text-center">Order Details</h2>
         <p class="mb-2"><span class="font-semibold">Order ID:</span> {{ $order->order_id }}</p>
-        <p class="mb-2"><span class="font-semibold">Order ID:</span> {{ $order->shop_name }}</p>
+        <p class="mb-2"><span class="font-semibold">Order ID:</span> {{ $order->shop->name }}</p>
         <p class="mb-2"><span class="font-semibold">Order ID:</span> {{ $order->timestamp }}</p>
         <p class="mb-2"><span class="font-semibold">Customer Name:</span> {{ $order->RecipientName }}</p>
         <p class="mb-4"><span class="font-semibold">Order Status:</span> {{ $order->order_status }}</p>
